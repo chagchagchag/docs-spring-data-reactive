1cde4d7c84ff76080f84f20b1dea27f0fc9e8501
chagchagchag <chagchagchag.dev@gmail.com>
2024-03-26 14:07:26 +0900

    first status

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs Spring Data Reactive</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-spring-data-reactive',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-spring-data-reactive',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
